b34a10c49e5696003f9abb83e8d64cf9e6a9b316
Zeyad Saleh <zeyadmostafasaleh@gmail.com>
2020-12-11 21:15:10 +0200

    fix rails admin style

---
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -0,0 +1,20 @@
+//= require_tree .
+$( "div" ).each(function() {
+    // $( this ).removeClass( "col-sm-offset-3 col-md-offset-2" );
+    alert("dsd")
+  });
+// $( ".col-sm-offset-3" ).removeClass( "col-sm-offset-3 col-md-offset-2" )
+// document.getAllElementsByClassName("col-sm-offset-3").classList.remove("col-sm-offset-3")
+// removeClass();
+// function removeClass(){
+//     var element1 = document.getAllElementsByClassName("col-sm-offset-3");
+//     element.classList.remove("col-sm-offset-3");
+  
+//    var element2 = document.getAllElementsByClassName("col-md-offset-2");
+//     element.classList.remove("col-md-offset-2");
+  
+//     alert(":dsa")
+//     alert(element1);
+//     alert(element2);
+// }
+
